62ffa9ac595eeeac111f86aacf85d76ec8fb8c62
Darau <daraualexandru@gmail.com>
2020-04-26 23:14:07 +0300

    first commit

---
+++ src/main/java/org/fis/proiect/Controllers/Controller.java
@@ -0,0 +1,31 @@
+package org.fis.proiect.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+
+public class Controller {
+
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Client", "Admin");
+    }
+
+    @FXML
+    public void handleRegisterAction() { ;
+
+
+    }
+}
